a55500e93152abfbf6010d677a45c1795d2bd962
Rolando Ugalde <46721519+rolandougalde@users.noreply.github.com>
2026-02-07 19:18:13 -0600

    Update SQL queries and comments in exercises 6-10

--- code/select_&_order_by/excercises_6-10.sql
+++ code/select_&_order_by/excercises_6-10.sql
@@ -15,19 +15,19 @@ SELECT *
 FROM Employees
 WHERE LastName LIKE 'D%';
 
---8. To retrieve the last names of Employees whose first letter is D and that contain T.
+-- 8. To retrieve the last names of Employees whose first letter is D and that contain T.
 
 SELECT *
 FROM Employees
 WHERE LastName LIKE 'D%t%';
 
---9. List the information of the orders that have not been shipped (ShippedDate).
+-- 9. List the information of the orders that have not been shipped (ShippedDate).
 
 SELECT *
 FROM Orders
 WHERE ShippedDate IS NULL;
 
---10. List all the fields from the Suppliers table where the Region column is NULL.
+-- 10. List all the fields from the Suppliers table where the Region column is NULL.
 
 SELECT *
 FROM Suppliers
