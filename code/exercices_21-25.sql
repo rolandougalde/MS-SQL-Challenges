8091882c118d90312681fc0615b119a93920341c
Rolando Ugalde <46721519+rolandougalde@users.noreply.github.com>
2026-02-20 15:15:52 -0600

    Add SQL for sales evolution by category and year
    Added SQL queries to show sales evolution by product category for each year.

--- code/exercices_21-25.sql
+++ code/exercices_21-25.sql
@@ -68,4 +68,31 @@ INNER JOIN Categories T3 ON T3.CategoryID = T2.CategoryID
 GROUP BY T3.CategoryName
 ORDER BY 2 DESC
 
+--23. From the report obtained in response 21, show the evolution of sales by
+--product category grouped for each year of operations.
+
+-- Step 1: 
+
+SELECT
+	T3.CategoryName,
+	DATEPART(YEAR, T4.OrderDate) AS [Year]
+FROM [Order Details] T1
+INNER JOIN Products T2 ON T2.ProductID = T1.ProductID
+INNER JOIN Categories T3 ON T3.CategoryID = T2.CategoryID
+GROUP BY T3.CategoryName
+ORDER BY 2 DESC
+
+-- Solved:
+
+SELECT
+	T3.CategoryName,
+	DATEPART(YEAR, T4.OrderDate) AS [Year],
+	SUM(T1.Quantity * T1.UnitPrice) AS [Ammount]
+FROM [Order Details] T1
+INNER JOIN Products T2 ON T2.ProductID = T1.ProductID
+INNER JOIN Categories T3 ON T3.CategoryID = T2.CategoryID
+INNER JOIN Orders T4 ON T4.OrderID = T1.OrderID
+GROUP BY T3.CategoryName, T4.OrderDate
+ORDER BY 1, 2
+
 -- End
